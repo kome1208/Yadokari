6089f32df0e36d13630a4d75bcaf80e8d6147719
Roeegh <admin@roeegh.dev>
2023-02-16 11:49:08 -0600

    🌸 Removed depricated code for the 2nd time

--- events/selectInteraction.js
+++ events/selectInteraction.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: 'interactionCreate',
 	async execute(interaction) {
-		if (!interaction.isSelectMenu()) return;
+		if (!interaction.isStringSelectMenu()) return;
 		const command = interaction.client.selectCommands.get(interaction.customId);
 
 		if (!command) return await require('../messages/defaultSelectError').execute(interaction);
