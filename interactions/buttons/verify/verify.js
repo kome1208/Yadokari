d98f31b05354e81cdfd7fa96726f2bffffc63595
komei <kome1208@outlook.jp>
2023-02-28 14:38:39 +0900

    Add member verification

---
+++ interactions/buttons/verify/verify.js
@@ -0,0 +1,22 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	id: 'verify',
+	async execute(interaction) {
+        await interaction.deferReply({ ephemeral: true }).catch(() => {});
+        if (interaction.member.roles.cache.has('1079892872742043785')) return interaction.editReply('You are already verified!');
+        try {
+            await interaction.member.roles.add('1079892872742043785');
+            await interaction.editReply({ embeds: [
+                    new EmbedBuilder()
+                        .setTitle('Verified!')
+                        .setDescription('Your verification has completed successfully.')
+                        .setColor('Green')
+                ]
+            });
+        } catch (e) {
+            console.log(e);
+            await interaction.editReply('There was an error while trying to update your roles.')
+        }
+	}
+};
