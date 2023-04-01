0818b7f18634bae2829131106f50493f1cbe6ad9
kome1208 <kome1208@outlook.jp>
2023-04-02 00:40:41 +0900

    MEMBER IS NOT DEFINED :skull:

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -3,7 +3,7 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = {
 	name: 'guildBanAdd',
 	async execute(ban) {
-		let channel = await member.guild.channels.fetch('1075283803171455037');
+		let channel = await ban.guild.channels.fetch('1075283803171455037');
 		channel.send({ embeds: [
 			new EmbedBuilder()
 			.setTitle('Member Banned')
